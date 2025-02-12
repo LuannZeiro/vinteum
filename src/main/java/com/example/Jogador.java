2bf26ec8df2ff411b164dae50b7ac5b698bf5d18
Luann Mariano <luann.mariano2003@gmail.com>
2025-02-11 22:08:11 -0300

    primeiro commit

---
+++ src/main/java/com/example/Jogador.java
@@ -0,0 +1,11 @@
+package com.example;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Jogador {
+    private int pontos;
+    private List<Carta> cartas = new ArrayList<>();
+    private boolean parou = false;    
+}
